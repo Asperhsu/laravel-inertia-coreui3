cb207577b669facbd9f14d87a6716c95745426cb
Asper <asper@strategicsale.com>
2020-09-21 17:57:13 +0800

    add server side

---
+++ app/Providers/InertiaServiceProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Session;
+
+class InertiaServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerInertia();
+    }
+
+    public function registerInertia()
+    {
+        Inertia::version(function () {
+            return md5_file(public_path('mix-manifest.json'));
+        });
+
+        Inertia::share([
+            'auth' => function () {
+                return [
+                    'user' => auth()->check() ? auth()->user()->toArray() : null,
+                ];
+            },
+            'flash' => function () {
+                return [
+                    'success' => session('success'),
+                    'error' => session('error'),
+                ];
+            },
+            'sidebar' => function () {
+                return [
+                    ['name' => 'Dashboard', 'href' => url('/dashboard'), 'icon' => 'fas fa-tachometer-alt'],
+                    ['name' => 'About', 'href' => url('/about')],
+                ];
+            },
+        ]);
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
